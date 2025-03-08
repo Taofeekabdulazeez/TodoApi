2655feed6090bdbf1219533b49b18ae2407cf7df
Taofeek Abdulazeez <102738186+Taofeekabdulazeez@users.noreply.github.com>
2025-03-08 23:19:04 +0100

    Enable Swagger in production environment for API documentation

--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ var supabase_anon_key = builder.Configuration["Supabase:AnonKey"];
 builder.Services.AddControllers().AddNewtonsoftJson();
 builder.Services.AddScoped<Supabase.Client>(_ => new Supabase.Client(supabaseUrl, supabase_anon_key));
 builder.Services.AddScoped<TodosService>();
-// add swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(
     options =>
@@ -20,16 +19,13 @@ builder.Services.AddSwaggerGen(
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(
-        options =>
-        {
-            options.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
-        }
-    );
-}
+app.UseSwagger();
+app.UseSwaggerUI(
+    options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+    }
+);
 
 app.UseHttpsRedirection();
 
